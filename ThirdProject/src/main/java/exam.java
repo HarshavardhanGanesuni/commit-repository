689bcef3d0ad9b9bdc27681b28f514e93b99a5f0
firstuser <admin@yanasoftware.com>
2012-11-16 05:33:25 -0600

    commit testing

---
+++ ThirdProject/src/main/java/exam.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: indianeagle
+ * Date: 11/15/12
+ * Time: 5:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class exam {
+    public static void main(String[] args) {
+        System.out.println("*******thirdproject with class 1****************");
+    }
+}
---
+++ ThirdProject/src/main/java/example2.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: indianeagle
+ * Date: 11/15/12
+ * Time: 5:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class example2 {
+    public static void main(String[] args) {
+        System.out.println("************Secondclass of third project*****************");
+    }
+}
